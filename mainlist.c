af6d019235cde7b0a04cfd0cfdb370f4b3e742dd
TuffixTurtle <43189716+TuffixTurtle@users.noreply.github.com>
2020-04-12 21:13:50 -0700

    Create mainlist.c

---
+++ mainlist.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  linked_list
+//
+//  Created by William McCarthy on 064//20.
+//  Copyright © 2020 William McCarthy. All rights reserved.
+//
+
+#include "slist.h"
+#include "dlist.h"
+
+//-------------------------------------------------
+int main(int argc, const char* argv[]) {
+  slist_test();
+  dlist_test();
+
+  return 0;
+}
+
+/*Majority of code is cobyrighted to professor.*/
